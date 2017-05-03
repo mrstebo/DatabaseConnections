b45fdc8e908b1923faa37949e4ac048998c75451
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-05-03 14:29:56 +0100

    Added tests.

---
+++ src/DatabaseConnections.Postgres.Tests/PostgresDatabaseTests.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+using NUnit.Framework;
+
+namespace DatabaseConnections.Postgres.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class PostgresDatabaseTests
+    {
+        [Test]
+        public void Constructor_ShouldNot_ThrowException()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Assert.DoesNotThrow(() => new PostgresDatabase(new NpgsqlConnection()));
+        }
+    }
+}
