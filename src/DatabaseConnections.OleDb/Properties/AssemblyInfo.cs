3c767ee8bd1b05b2b0180beb1a94592ebfd83e1f
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-05-03 12:33:40 +0100

    Updated AssemblyInfo.

--- src/DatabaseConnections.OleDb/Properties/AssemblyInfo.cs
+++ src/DatabaseConnections.OleDb/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DatabaseConnections.OleDb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A wrapper around the standard IDb classes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mrstebo, ekmsystems")]
 [assembly: AssemblyProduct("DatabaseConnections.OleDb")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
