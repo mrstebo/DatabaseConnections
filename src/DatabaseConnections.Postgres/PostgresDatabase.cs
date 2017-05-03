d0fef7df181191cdcb0c1bcd60fbd38aa9c94def
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-05-03 14:25:37 +0100

    Added the DatabaseConnections.Postgres project.

---
+++ src/DatabaseConnections.Postgres/PostgresDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+
+namespace DatabaseConnections.Postgres
+{
+    public class PostgresDatabase : Database
+    {
+        public PostgresDatabase(NpgsqlConnection connection)
+            : this(new NpgsqlConnectionWrapper(connection))
+        {
+        }
+
+        internal PostgresDatabase(IDbConnectionWrapper connection) 
+            : base(connection)
+        {
+        }
+    }
+}
