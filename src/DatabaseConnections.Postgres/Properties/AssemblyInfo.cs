55762ddf0cd71d6e4bd7d2a541a05c279f55d1e1
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-05-03 14:29:50 +0100

    Updated to .NET 4.5. Updated AssemblyInfo.

--- src/DatabaseConnections.Postgres/Properties/AssemblyInfo.cs
+++ src/DatabaseConnections.Postgres/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DatabaseConnections.Postgres")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A wrapper around the standard IDb classes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mrstebo, ekmsystems")]
 [assembly: AssemblyProduct("DatabaseConnections.Postgres")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("DatabaseConnections.Postgres.Tests")]
