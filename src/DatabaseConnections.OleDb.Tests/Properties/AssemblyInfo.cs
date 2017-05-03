8df2bcdd68f561ad2bad9e22ed1c15b54fa54f7a
Steven Atkinson <stevenatkinson@ekmsystems.co.uk>
2017-05-03 12:32:47 +0100

    Added tests.

--- src/DatabaseConnections.OleDb.Tests/Properties/AssemblyInfo.cs
+++ src/DatabaseConnections.OleDb.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DatabaseConnections.OleDb")]
+[assembly: AssemblyTitle("DatabaseConnections.OleDb.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DatabaseConnections.OleDb")]
+[assembly: AssemblyProduct("DatabaseConnections.OleDb.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c615f7f-d944-44fd-8e1c-32eace0ca7ad")]
+[assembly: Guid("7505ee85-1fa6-47fa-85f6-c23fc1be2aa5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("DatabaseConnections.OleDb.Tests")]
